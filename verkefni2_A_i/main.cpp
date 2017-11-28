8f2bafc51d171cff70bd514d84ec3a0f40143e14
Viktor Ingi Kárason <vingik@gmail.com>
2017-11-28 22:28:50 +0000

    breytingar á main.cpp

--- verkefni2_A_i/main.cpp
+++ verkefni2_A_i/main.cpp
@@ -1,12 +1,56 @@
 #include <iostream>
 #include <string>
 #include "superhero.h"
+#include <fstream>
 
 using namespace std;
 
+void read_file();
+void insert_into_file(superhero& hero);
+
 int main()
 {
-    superhero larry("larry",42,'Q');
-    cout << larry.get_age() << endl;
+    superhero hero;
+    hero.create_hero(hero);
+    insert_into_file(hero);
+    read_file();
     return 0;
+}
+
+void read_file(){
+    ifstream fin;
+    superhero hero;
+    fin.open("heroFile.txt");
+
+    if(fin.is_open())
+    {
+        while(!fin.eof())
+        {
+            fin >> hero;
+            if(fin.eof())
+                break;
+            cout << hero;
+        }
+    }
+    else
+    {
+        cout << "Could not open file";
+    }
+}
+
+void insert_into_file(superhero& hero){
+    ofstream fout;
+    fout.open("heroFile.txt", ios::app);
+
+    if(fout.is_open())
+    {
+        hero.setVerbose(false);
+        fout << hero;
+        fout.close();
+        hero.setVerbose(true);
+    }
+    else
+    {
+        cout << "Could not open file";
+    }
 }
