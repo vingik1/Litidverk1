ffadebb611125ff412e887955e2399ef792c2cdf
Viktor Ingi Kárason <vingik@gmail.com>
2017-11-28 22:30:31 +0000

    changes to class

--- verkefni2_A_i/include/superhero.h
+++ verkefni2_A_i/include/superhero.h
@@ -11,20 +11,22 @@ class superhero
     public:
         superhero();
         superhero(string name,int age, char superpower);
-        string get_name();
+        /*string get_name();
         int get_age();
         char get_superpower();
         string set_name(string name);
         int set_age(int age);
-        char set_superpower(char superpower);
-        string superheropower(char superpower);
-        friend ostream& operator << (ostream& out, const SuperHero& superhero);
-        friend istream& operator >> (istream& in, SuperHero& superhero);
+        char set_superpower(char superpower);*/
+        friend ostream& operator << (ostream& out, const superhero& hero);
+        friend istream& operator >> (istream& in, superhero& hero);
+        void create_hero(superhero& hero);
+        void setVerbose(bool v);
 
     private:
         string _name;
         int _age;
         char _superpower;
+        bool verbose;
 };
 
 #endif // SUPERHERO_H
